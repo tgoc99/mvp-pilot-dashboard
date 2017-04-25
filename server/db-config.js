b171a712f5f852f3d1e839687f21c959a7f4c07b
Tommy O'Connor <tgoc99@gmail.com>
2017-04-25 15:37:54 -0400

    heroku

--- server/db-config.js
+++ server/db-config.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var config = require('../config.js');
 
-
 //mongoose.connect('mongodb://localhost/pilotdb');
 mongoose.connect(`mongodb://${config.user}:${config.pass}@ds119151.mlab.com:19151/mvp`);
 var db = mongoose.connection;
