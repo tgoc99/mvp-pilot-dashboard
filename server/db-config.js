2581e40e9503613b7b869074793dc83b20251908
Tommy O'Connor <tgoc99@gmail.com>
2017-04-25 15:24:52 -0400

    updating to deploy

--- server/db-config.js
+++ server/db-config.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var config = require('../config.js');
 
-mongoose.connect('mongodb://localhost/pilotdb');
+
+//mongoose.connect('mongodb://localhost/pilotdb');
+mongoose.connect(`mongodb://${config.user}:${config.pass}@ds119151.mlab.com:19151/mvp`);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
